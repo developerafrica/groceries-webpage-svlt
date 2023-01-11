a1119864317e6ecc84429f04d7aea04e09c7206c
peter butao <peterethanbutao@gmail.com>
2023-01-11 03:50:48 -0800

    commit 1
    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,80 @@
+<script>
+  import "../app.scss";
+</script>
+<div class="layout">
+  <div class="lay-out">
+    <header>
+      <div class="brand">
+        <div class="logo">
+          <img src="/logo.png" alt="logo">
+        </div>
+        <div class="text">
+          <h1>
+            DEVELOPER AFRICA MW
+          </h1>
+        </div>
+      </div>
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <div class="ft"><a href="https://grocerylistmw.netlify.app">LAUNCH APP</a></div>
+    </footer>
+  </div>
+</div>
+<style lang="scss">
+  @mixin fnt ($c,$s,$w) {
+    font-size: $s;
+    color: $c;
+    font-weight: $w;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;  
+}
+  
+  .layout{
+    .lay-out{
+      header{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: 10;
+        background: var(--gn);
+        .brand{
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          padding: 10px 10px          
+        }
+        .logo{
+          img{
+            width: 30px;
+          }
+        }
+        .text{
+          h1{
+            @include fnt(var(--wt), 0.8rem, 500)
+          }
+        }
+      }
+      footer{
+        text-align: center;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        .ft{
+          padding: 10px;
+          a{
+            background: var(--gn);
+            border-radius: 5px;
+            padding: 15px 0;
+            display: block;
+            @include fnt(var(--wt), 1rem, 450);
+            text-decoration: none;
+          }
+        }
+      }
+    }
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<article class="main">
+    <div class="art-main">
+        <main>
+            <img src="/phonedesign.png" alt="phone design">
+        </main>
+        <footer>
+            <div class="text">
+                <p>do you still write your groceries on paper ?</p>
+                <h1>this app is for you</h1>
+            </div>
+        </footer>
+    </div>
+</article>
+<style lang="scss">
+    @mixin fnt ($c,$s,$w) {
+    font-size: $s;
+    color: $c;
+    font-weight: $w;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    
+    
+}
+    article{
+        .art-main{
+            padding: 10vh 0 0 0;
+            main{
+                margin: auto;
+                width: 300px;
+                img{
+                    margin: auto;
+                    width: 100%;
+                    padding: 10px;
+                }
+            }
+            footer{
+                padding: 10px;
+                text-align: center;
+                .text{
+                    p{
+                        @include fnt(var(--tc), 0.78rem, 500);
+                    }
+                    h1{
+                        @include fnt(var(--tc), 0.9rem, 800);
+                        text-transform: uppercase;
+                        letter-spacing: 1.5px;
+                    }
+                }
+
+            }
+
+        }
+    }
+</style>
